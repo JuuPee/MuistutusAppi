c623f4f5f439eead8168d23264f6b16875bd1592
JP <juhapekka.tiirikainen@gmail.com>
2018-11-15 11:32:12 +0200

    First initial commit

---
+++ app/src/main/java/com/example/jp/muistutusappi/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.jp.muistutusappi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JP on 11/15/2018.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
